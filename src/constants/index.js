2b0b1898a33df92624df0ef058d5e429045ef385
unknown <alimalikali1928@gmail.com>
2024-11-08 13:21:04 -0800

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,289 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Ayesha Khan',
+    position: 'Editor at Pakistan News Network',
+    img: 'assets/review1.png',
+    review:
+      'Working with Ali was an outstanding experience! He developed our news channel website with a clean, professional design, and it’s incredibly user-friendly. His commitment to delivering on time and attention to detail truly impressed us. Highly recommend him for any media-focused web projects.',
+  },
+  {
+    id: 2,
+    name: 'Ahmed Rehman',
+    position: 'Owner of Rehman Electronics',
+    img: 'assets/review2.png',
+    review:
+      'Ali helped us launch our e-commerce platform, and the results have been fantastic. Our sales and customer engagement have increased significantly. He’s a true expert, delivering a seamless, scalable site that’s easy for customers to navigate. A pleasure to work with!',
+  },
+  {
+    id: 3,
+    name: 'Zartash Malik',
+    position: 'Project Lead at Digital Karachi',
+    img: 'assets/review3.png',
+    review:
+      'Ali was exceptional! He transformed our vision for an online news platform into a reality. His technical skill and understanding of media-centric needs set him apart, and he was responsive throughout the process. I highly recommend his services!',
+  },
+  {
+    id: 4,
+    name: 'Emily Johnson',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review4.png',
+    review:
+      'Ali delivered above and beyond on our e-commerce site. From design to development, he managed everything with professionalism and skill. The site is both robust and easy to manage, and our sales have improved significantly. Working with him was an excellent experience!',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'E-Commerce Full Stack Application',
+    desc: 'Developed a complete e-commerce solution using React.js, Node.js, Express.js, and MongoDB. Integrated SCSS for styling, Firebase for authentication, and Stripe for secure payments, prioritizing scalability and seamless user experience.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Application is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Personal Portfolio',
+    desc: 'Created a visually engaging personal portfolio to showcase skills and projects, utilizing Next.js, Tailwind CSS, and Framer Motion for smooth animations. Deployed on Vercel for optimal performance and accessibility.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Application is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Task Master & Authentication App',
+    desc: 'Developed a task management application with secure authentication features using the MERN stack. Designed for productivity, this app allows users to efficiently organize tasks and access them across devices. Deployed on Vercel for easy access.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Application is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'React Native Video Uploader App',
+    desc: 'Built a mobile application in React Native, enabling users to upload and manage videos easily. Integrated Appwrite as a backend solution, optimizing media handling and ensuring a streamlined user experience.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Application is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  // {
+  //   title: 'Imaginify - AI Photo Manipulation App',
+  //   desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+  //   subdesc:
+  //     'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+  //   href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+  //   texture: '/textures/project/project5.mp4',
+  //   logo: '/assets/project-logo5.png',
+  //   logoStyle: {
+  //     backgroundColor: '#1C1A43',
+  //     border: '0.2px solid #252262',
+  //     boxShadow: '0px 0px 60px 0px #635BFF4D',
+  //   },
+  //   spotlight: '/assets/spotlight5.png',
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: 'React.js',
+  //       path: '/assets/react.svg',
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'TailwindCSS',
+  //       path: 'assets/tailwindcss.png',
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'TypeScript',
+  //       path: '/assets/typescript.png',
+  //     },
+  //     {
+  //       id: 4,
+  //       name: 'Framer Motion',
+  //       path: '/assets/framer.png',
+  //     },
+  //   ],
+  // },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-1, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Hybrid Solutions',
+    pos: 'Full Stack Developer',
+    duration: '2024 - Present',
+    title: "At Hybrid Solutions, I engineered a high-performance news website using Vue.js and Tailwind CSS, focusing on optimizing load times and user experience. Additionally, I revamped the UI of a property dealer platform, Qwesty, using Next.js and Tailwind CSS, resulting in a smoother and more engaging user interface.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Cybil Tech',
+    pos: 'Front-end Developer',
+    duration: '2022 - 2023',
+    title: "As a front-end developer at Cybil Tech, I developed two interactive, user-centric websites using React.js and Tailwind CSS, prioritizing responsive design and seamless user interactions. My work contributed to enhancing the online presence and engagement of the client platforms.",
+    icon: '/assets/framer.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 4,
+    name: 'Academic Projects',
+    pos: 'Developer',
+    duration: '2019 - 2020',
+    title: "During my academic studies, I completed notable projects such as a React Native Video Uploader app with Appwrite integration, emphasizing media handling, and a Task Master app using the MERN stack, incorporating user authentication for a comprehensive task management solution.",
+    icon: '/assets/framer.svg',
+    animation: 'salute',
+  },
+];
+
